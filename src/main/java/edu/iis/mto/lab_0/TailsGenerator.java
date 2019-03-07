e5ef184824c74d9a6c8482f1d59d7ccf70dcd14d
209434 <209434@edu.p.lodz.pl>
2019-03-07 18:05:38 +0100

    tailsShouldReturnEmptyListIfParameterIsNull passed

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,12 +1,17 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
+
+        if( value == null) {
+            return new ArrayList<>();
+        }
+
         return new ArrayList<>(value.length());
     }
-
 }
